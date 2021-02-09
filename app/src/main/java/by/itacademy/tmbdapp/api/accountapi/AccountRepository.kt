8eb42097e1bf07fa6a2a9cd6dab9170aafd6aa23
Igor Ivanov <guver3g@gmail.com>
2021-02-09 21:53:20 +0300

    rebuild accountPresenterImpl

--- app/src/main/java/by/itacademy/tmbdapp/api/accountapi/AccountRepository.kt
+++ app/src/main/java/by/itacademy/tmbdapp/api/accountapi/AccountRepository.kt
@@ -7,10 +7,8 @@ class AccountRepository {
     private val accountApi: AccountApi =
         RetrofitRepository.getRetrofit().create(AccountApi::class.java)
 
-
     fun getAccountDetails(sessionId: String?) = accountApi.getAccountDetails(sessionId = sessionId)
         .subscribeOn(Schedulers.newThread())
-
 
     fun getAccountRatedMovies(sessionId: String?) =
         accountApi.getAccountRatedMovies(sessionId = sessionId)
---
+++ app/src/main/java/by/itacademy/tmbdapp/model/AccountDetailsModel.kt
@@ -0,0 +1,6 @@
+package by.itacademy.tmbdapp.model
+
+class AccountDetailsModel (
+    val model: AccountModel,
+    val list: List<String>,
+)
